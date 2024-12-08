4c1c798ef4f2443fbb9f25fbea13c63fffc44df8
jonathansickert <sickert.jonathan@gmail.com>
2024-12-08 14:35:02 +0100

    removed pandas deps

--- aoc24/day01.py
+++ aoc24/day01.py
@@ -1,23 +1,10 @@
-import numpy as np
-import pandas as pd
-
 from aoc24.input import read_input_lines
 
 lines: list[str] = read_input_lines(day=1)
-lines_split: list[list[str]] = [line.split() for line in lines]
-answer_1: int = 0
-answer_2: int = 0
-
-df = pd.DataFrame(data=lines_split, columns=["left", "right"])
-
-left = df["left"].astype(int).sort_values(ignore_index=True)
-right = df["right"].astype(int).sort_values(ignore_index=True)
-answer_1 = np.abs(left - right).sum()
-
-right_value_counts = right.value_counts()
-left_in_right = right_value_counts.index.intersection(left)  # type: ignore
-left_in_right_counts = right_value_counts[left_in_right]
-answer_2 = (left_in_right_counts * left_in_right).sum()
+left: list[int] = sorted([list(map(int, line.split()))[0] for line in lines])
+right: list[int] = sorted([list(map(int, line.split()))[1] for line in lines])
+answer_1: int = sum([abs(left[i] - right[i]) for i in range(len(left))])
+answer_2: int = sum([left[i] * right.count(left[i]) for i in range(len(left))])
 
 print(answer_1)
 print(answer_2)
