297d7745f5ca5c6038e21375c2d980f640548d05
jonathansickert <sickert.jonathan@gmail.com>
2024-12-09 23:55:44 +0100

    code beautification

--- aoc24/day06.py
+++ aoc24/day06.py
@@ -3,11 +3,6 @@ from copy import deepcopy
 from aoc24.input import read_input_lines
 
 input: list[str] = read_input_lines(day=6)
-grid: list[str] = (
-    ["@" * (len(input[0]) + 2)]
-    + ["@" + line + "@" for line in input]
-    + ["@" * (len(input[0]) + 2)]
-)
 step_lookup: dict[str, tuple[int, int, str]] = {
     "^": (-1, 0, ">"),
     ">": (0, 1, "V"),
@@ -18,72 +13,61 @@ answer_1: int = 0
 answer_2: int = 0
 
 
-def walk(x: int, y: int, facing: str, grid: list[str]) -> tuple[int, int, str] | None:
-    dx, dy, new_facing = step_lookup[facing]
-    obstacle: str = grid[x + dx][y + dy]
-    if obstacle == "@":  # end of the map
-        return None
-    if obstacle == "#":  # facing an obstacle
-        return x, y, new_facing
-    if obstacle == ".":  # facing no obstacle
-        return x + dx, y + dy, facing
+def isinbound(x: int, y: int, grid: list[str]) -> bool:
+    return x < len(grid) and x >= 0 and y < len(grid[0]) and y >= 0
 
 
-def find_start(grid: list[str]) -> tuple[int, int]:
+def find_start(grid: list[str]) -> tuple[int, int, str]:
     for x in range(len(grid)):
         for y in range(len(grid[0])):
-            if grid[x][y] == "^":
-                grid[x] = grid[x][:y] + "." + grid[x][y + 1 :]
-                return x, y
-    return (-1, -1)
-
-
-def is_loop(start_x: int, start_y: int, facing: str, grid: list[str]) -> bool:
-    steps: set = set()
-    cur_x, cur_y, cur_facing = start_x, start_y, facing
-    while 1:
-        steps.add((cur_x, cur_y, cur_facing))
-        new_step = walk(cur_x, cur_y, cur_facing, grid)
-        if new_step is None:
-            break
-        cur_x, cur_y, cur_facing = new_step
-        if (cur_x, cur_y, cur_facing) in steps:
-            return True  # detected loop
-    return False
+            if (dir := grid[x][y]) in {"^", ">", "V", "<"}:
+                return x, y, dir
+    raise Exception
 
 
 def place_obstacle_ahead(
-    cur_x: int, cur_y: int, facing: str, grid: list[str]
+    x: int, y: int, dir: str, grid: list[str]
 ) -> tuple[int, int] | None:
-    dx, dy, _ = step_lookup[facing]
-    x, y = cur_x + dx, cur_y + dy
-    if grid[x][y] == ".":
-        grid[x] = grid[x][:y] + "#" + grid[x][y + 1 :]
-        return x, y
-    return None
+    dx, dy, _ = step_lookup[dir]
+    ox, oy = x + dx, y + dy
+    if not isinbound(ox, oy, grid) or grid[ox][oy] != ".":
+        return None
+    grid[ox] = grid[ox][:oy] + "#" + grid[ox][oy + 1 :]
+    return ox, oy
 
 
-def run(start_x: int, start_y: int, grid: list[str]) -> tuple[int, int]:
-    steps: set = set()
-    loop_obstacles: set = set()
-    cur_x, cur_y = start_x, start_y
-    cur_facing: str = "^"
-    while 1:
-        steps.add((cur_x, cur_y))
-        test_grid: list[str] = deepcopy(grid)
-        new_obstacle = place_obstacle_ahead(cur_x, cur_y, cur_facing, test_grid)
-        if new_obstacle is not None:
-            if is_loop(start_x, start_y, "^", test_grid) is True:
-                loop_obstacles.add(new_obstacle)
-        new_step = walk(cur_x, cur_y, cur_facing, grid)
-        if new_step is None:
-            break
-        cur_x, cur_y, cur_facing = new_step
-    return len(steps), len(loop_obstacles)
+def simulate_run(
+    start_x: int, start_y: int, start_dir: str, grid: list[str]
+) -> tuple[bool, set[tuple[int, int, str]]]:
+    path = set()
+    x, y, dir = start_x, start_y, start_dir
+
+    while True:
+        if (x, y, dir) in path:
+            return False, path  # loop
+        path.add((x, y, dir))
+        dx, dy, new_dir = step_lookup[dir]
+        new_x, new_y = x + dx, y + dy
+        if not isinbound(new_x, new_y, grid):
+            return True, path  # exit
+        if grid[new_x][new_y] == "#":
+            dir = new_dir
+        else:
+            x, y = new_x, new_y
 
 
-start_x, start_y = find_start(grid)
-answer_1, answer_2 = run(start_x, start_y, grid)
+start_x, start_y, start_dir = find_start(input)
+_, path = simulate_run(start_x, start_y, start_dir, input)
+loop_obstacle_positions = set()
+for step in path:
+    new_grid: list[str] = deepcopy(input)
+    if (pos := place_obstacle_ahead(*step, new_grid)) is not None:
+        success, _ = simulate_run(start_x, start_y, start_dir, new_grid)
+        if success is False:
+            loop_obstacle_positions.add(pos)
+
+answer_1 = len({(step[0], step[1]) for step in path})
+answer_2 = len(loop_obstacle_positions)
 
 print(answer_1)
 print(answer_2)
