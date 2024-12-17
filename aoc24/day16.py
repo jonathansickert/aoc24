70945b0a4fc493c9924f18ca5a828943b8534a28
jonathansickert <sickert.jonathan@gmail.com>
2024-12-17 19:00:07 +0100

    some speed improvement

--- aoc24/day16.py
+++ aoc24/day16.py
@@ -1,3 +1,6 @@
+import heapq
+import sys
+
 from aoc24.aoc_decorator import solves_puzzle
 
 
@@ -25,34 +28,130 @@ def get_new_dist(cur_dx: int, cur_dy: int, dx: int, dy: int) -> int:
     return max(x_diff, y_diff) * 1000
 
 
-def dijkstra(V: set[tuple[int, int]], S: tuple[int, int]) -> dict[tuple[int, int], int]:
+def dijkstra(V: set[tuple[int, int]], S: tuple[int, int]):
     dist: dict[tuple[int, int], int] = {}
-    prev: dict[tuple[int, int], tuple[int, int] | None] = {}
-    dir: dict[tuple[int, int], tuple[int, int]] = {S: (0, 1)}
-    queue: set[tuple[int, int]] = set()
-    for v in V:
-        dist[v] = float("inf")
-        prev[v] = None
-        queue.add(v)
-        # heapq.heappush(Q, (float("inf"), v))
-    dist[S] = 0
+    prev: dict[tuple[int, int], list[tuple[int, int]]] = {}
+    dir: dict[tuple[int, int], tuple[int, int]] = {}
 
-    while len(queue) > 0:
-        u = get_min_dist(dist, queue)
-        queue.remove(u)
-        # _, u = heapq.heappop(Q)
-        # print(u)
+    for v in V:
+        dist[v] = sys.maxsize
+        prev[v] = []
+    dist[S] = 0
+    dir[S] = (0, 1)
+
+    Q = []
+    heapq.heappush(Q, (0, S))
+
+    while Q:
+        du, u = heapq.heappop(Q)
         dx, dy = dir[u]
+
+        if du > dist[u]:
+            continue
+
         for ndx, ndy in {(1, 0), (-1, 0), (0, 1), (0, -1)}:
-            if (n := (u[0] + ndx, u[1] + ndy)) in queue:
-                new_dist: int = dist[u] + get_new_dist(dx, dy, ndx, ndy) + 1
-                # new_dist: int = dist[u] + 1
+            n = (u[0] + ndx, u[1] + ndy)
+            if n in V:
+                new_dist: int = du + get_new_dist(dx, dy, ndx, ndy) + 1
                 if new_dist < dist[n]:
                     dist[n] = new_dist
-                    prev[n] = u
                     dir[n] = (ndx, ndy)
-                    # heapq.heapreplace()
-    return dist
+                    heapq.heappush(Q, (new_dist, n))
+                    prev[n] = [u]
+                elif new_dist == dist[n]:
+                    prev[n].append(u)
+                    # print("equal: ", prev[n])
+        # print()
+    return dist, prev
+
+
+# def dijkstra2(V: set[tuple[int, int]], S: tuple[int, int], E):
+#     dist: dict[tuple[int, int], int] = {}
+#     prev = {}
+#     dir: dict[tuple[int, int], tuple[int, int]] = {S: (0, 1)}
+#     Q = set()
+#     for v in V:
+#         dist[v] = float("inf")
+#         prev[v] = []
+#         Q.add(v)
+#     dist[S] = 0
+#     prev[v] = None
+
+#     while len(Q) > 0:
+#         u = get_min_dist(dist, Q)
+#         Q.remove(u)
+#         dx, dy = dir[u]
+#         for ndx, ndy in {(1, 0), (-1, 0), (0, 1), (0, -1)}:
+#             n = (u[0] + ndx, u[1] + ndy)
+#             if n in V:
+#                 new_dist: int = dist[u] + get_new_dist(dx, dy, ndx, ndy) + 1
+#                 if new_dist <= dist[n]:
+#                     dist[n] = new_dist
+#                     dir[n] = (ndx, ndy)
+#                     prev[n].append(u)
+#                     print(prev[n])
+#     return prev
+
+
+# def dijkstra(V: set[tuple[int, int]], S: tuple[int, int]) -> dict[tuple[int, int], int]:
+#     dist: dict[tuple[int, int], int] = {}
+#     prev: dict[tuple[int, int], tuple[int, int] | None] = {}
+#     dir: dict[tuple[int, int], tuple[int, int]] = {S: (0, 1)}
+#     queue: set[tuple[int, int]] = set()
+#     for v in V:
+#         dist[v] = float("inf")
+#         prev[v] = None
+#         queue.add(v)
+#         # heapq.heappush(Q, (float("inf"), v))
+#     dist[S] = 0
+
+#     while len(queue) > 0:
+#         u = get_min_dist(dist, queue)
+#         queue.remove(u)
+#         # _, u = heapq.heappop(Q)
+#         # print(u)
+#         dx, dy = dir[u]
+#         for ndx, ndy in {(1, 0), (-1, 0), (0, 1), (0, -1)}:
+#             if (n := (u[0] + ndx, u[1] + ndy)) in queue:
+#                 new_dist: int = dist[u] + get_new_dist(dx, dy, ndx, ndy) + 1
+#                 # new_dist: int = dist[u] + 1
+#                 if new_dist < dist[n]:
+#                     dist[n] = new_dist
+#                     prev[n] = u
+#                     dir[n] = (ndx, ndy)
+#                     # heapq.heapreplace()
+#     return dist
+
+
+def path_length(
+    prev: dict[tuple[int, int], list[tuple[int, int]]],
+    E: tuple[int, int],
+) -> int:
+    visited = []
+
+    def walk_path_recursive(cur: tuple[int, int]):
+        prevs = prev.get(cur)
+        # print()
+        if prevs is None or len(prevs) == 0:
+            return
+        for p in prevs:
+            visited.append(p)
+            walk_path_recursive(p)
+
+    walk_path_recursive(E)
+    return len(visited)
+
+
+def walk_path(E: tuple[int, int], prev: dict[tuple[int, int], tuple[int, int]]):
+    path = []
+    cur: tuple[int, int] = E
+    while 1:
+        path.insert(0, cur)
+        next = prev.get(cur)
+        if next is None:
+            break
+        cur = next
+    return path
 
 
 @solves_puzzle(day=16, part=1)
@@ -72,7 +171,18 @@ def solve_part_1(input: str):
                 E = (x, y)
                 V.add(E)
     # print(len(V))
-    dist: dict[tuple[int, int], int] = dijkstra(V, S)
+    dist, prev = dijkstra(V, S)
+    # print(path_length(prev, E))
+    # prevs_len = {k: len(v) for k, v in prev.items() if len(v) > 1}
+    # print(prevs_len)
+    # for x in range(len(grid)):
+    #     for y in range(len(grid[0])):
+    #         if (x, y) in path:
+    #             print("X", end="")
+    #         else:
+    #             print(grid[x][y], end="")
+    #     print()
+
     return dist[E]
 
 
